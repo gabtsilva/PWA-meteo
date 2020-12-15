1f011174b2d8db529f1e5ecea62ce436c2683e46
Gabriel Torres S <gabriel.torres.silva@hotmail.com>
2020-12-15 19:12:33 +0100

    PWA + Fix bugs

---
+++ sw.js
@@ -0,0 +1,28 @@
+//https://codelabs.developers.google.com/codelabs/offline/#6 
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('cache-man').then(function(cache) {
+        return cache.addAll([
+          '/',
+          'images/icon.png',
+          'images/icons.png',
+          'index.html',
+          'scss/bootstrap.min.css',
+          'scss/style.css',
+          'js/jquery-3.5.1.min.js',
+          'js/index.js',
+        ]);
+      })
+    );
+   });
+  
+  //https://codelabs.developers.google.com/codelabs/offline/#7
+  self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
